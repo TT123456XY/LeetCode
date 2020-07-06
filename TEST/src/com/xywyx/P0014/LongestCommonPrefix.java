f85670bf6796e0ca43e362f10559f6d4b34e1f94
xy <sprite_xy@outlook.com>
2020-07-06 17:10:54 +0800

    P0014

---
+++ TEST/src/com/xywyx/P0014/LongestCommonPrefix.java
@@ -0,0 +1,44 @@
+package src.com.xywyx.P0014;
+
+/**
+ * 编写一个函数来查找字符串数组中的最长公共前缀。
+ * 如果不存在公共前缀，返回空字符串 ""。
+ * 示例 1:
+ * 输入: ["flower","flow","flight"]
+ * 输出: "fl"
+ * 示例 2:
+ * 输入: ["dog","racecar","car"]
+ * 输出: ""
+ * 解释: 输入不存在公共前缀。
+ */
+public class LongestCommonPrefix {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        String res = s.longestCommonPrefix(null);
+        System.out.println(res);
+    }
+}
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        String prefix = strs[0];
+        int count = strs.length;
+        for (int i = 1; i < count; i++) {
+            prefix = longestCommonPrefix(prefix, strs[i]);
+            if (prefix.length() == 0) {
+                break;
+            }
+        }
+        return prefix;
+    }
+    private String longestCommonPrefix(String str1, String str2) {
+        int length = Math.min(str1.length(), str2.length());
+        int index = 0;
+        while (index < length && str1.charAt(index) == str2.charAt(index)) {
+            index++;
+        }
+        return str1.substring(0, index);
+    }
+}
