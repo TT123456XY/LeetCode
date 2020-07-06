9e603f7dd991f2fa9c7cdcb9c7b0f4fc3f255955
TT123456XY <sprite_xy@outlook.com>
2020-07-06 22:20:29 +0800

    a

---
+++ TEST/src/com/xywyx/P0015/ThreeSum.java
@@ -0,0 +1,15 @@
+package com.xywyx.P0015;
+
+import java.util.List;
+
+public class ThreeSum {
+    Solution s = new Solution();
+    List<List<Integer>> res = s.threeSum(null);
+}
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {                   
+
+
+
+    }
+}
